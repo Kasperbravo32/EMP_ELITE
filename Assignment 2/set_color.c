a7fe867ddefbe4a678a38e58179b580673862614
Lars Nørgaard Larsen <lars@ljpc.dk>
2017-03-10 15:55:04 +0100

    Change
    To see what changes it makes

--- Assignment 2/set_color.c
+++ Assignment 2/set_color.c
@@ -3,7 +3,7 @@
 * MODULENAME.: SET_LED.c
 *
 * PROJECT....: Assignment 2
-*
+* This is a change
 * DESCRIPTION: See module specification file (.h-file).
 *
 * Change Log: ************************************************************/
