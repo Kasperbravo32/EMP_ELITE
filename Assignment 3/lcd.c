fb7e54d5c44f9b3aed8095ca051f75dc6ccdb82f
Lars Nørgaard Larsen <lars@ljpc.dk>
2017-03-17 12:50:39 +0100

    IT FUCKING WORKING LIKE OMG

--- Assignment 3/lcd.c
+++ Assignment 3/lcd.c
@@ -9,14 +9,15 @@
 #define LCD_RS                  0x04    // PD2
 #define LCD_E                   0x08    // PD3
 
-#define LCD_CLEAR_DISPLAY       0x01    // Clear display
-#define LCD_DISPLAY_OFF         0x08    // Display off
-#define LCD_DISPLAY_ON_BLINK    0x0C    // Display on with blinking cursor
 #define LCD_FUNCTIONRESET       0x03    // Function reset
 #define LCD_ENABLE_4BIT         0x02    // Enable 4bit mode
 #define LCD_FUNCTION_SET        0x28    // 4 bit mode, 1/16, 5x8 font
+#define LCD_DISPLAY_OFF         0x08    // Display off
+#define LCD_DISPLAY_ON_BLINK    0x0F    // Display on with blinking cursor
 #define LCD_ENTRYMODE           0x06    // Entry mode 
-#define LCD_HOME                0x01    // Home
+#define LCD_HOME                0x02    // Home
+#define LCD_CLEAR_DISPLAY       0x01    // Clear display
+
 //******************* Function declaration *******************//
 void wait_micro(int time);
 void lcd_write8(int value);
@@ -31,16 +32,28 @@ void wait( int time );
 // This function sets up the LCD display for the EMP board
 int main(void)
 {
-
     lcd_pinsetup();     //Setting up pins
+    GPIO_PORTF_DATA_R &= ~(0x0E);          //slukker RS og Enable
 
     lcd_init();         //Initializing LCD
 
     //Clear display
     GPIO_PORTD_DATA_R &= ~(LCD_RS);
     lcd_write8(LCD_CLEAR_DISPLAY);
-    wait_micro(10);
-    lcd_write(0b01010101);
+    wait_micro(1000);
+
+
+    lcd_write('T');
+    lcd_write('I');
+    lcd_write('S');
+    lcd_write('S');
+    lcd_write('E');
+    lcd_write('M');
+    lcd_write('Y');
+    lcd_write('N');
+    lcd_write('D');
+    wait(100);
+    GPIO_PORTF_DATA_R |= 0x02;
 
     while(1);
 
@@ -49,29 +62,35 @@ int main(void)
 //******************* End of function *******************//
 
 //******************* HARDWARE SETUP *******************//
+void lcd_write_string()
+
 void lcd_pinsetup(void)
 {
 
     int dummy;
     // Enable the GPIO port that is used for the LCD Display.
-    SYSCTL_RCGC2_R |= (SYSCTL_RCGC2_GPIOC | SYSCTL_RCGC2_GPIOD) ;
+    SYSCTL_RCGC2_R |= (SYSCTL_RCGC2_GPIOC | SYSCTL_RCGC2_GPIOD | SYSCTL_RCGC2_GPIOF) ;
 
     // Do a dummy read to insert a few cycles after enabling the peripheral.
     dummy = SYSCTL_RCGC2_R;
 
     GPIO_PORTC_LOCK_R = 0x4C4F434B;  // Unlock the CR register
     GPIO_PORTD_LOCK_R = 0x4C4F434B;  // Unlock the CR register
+    GPIO_PORTF_LOCK_R = 0x4C4F434B;  // Unlock the CR register
 
     GPIO_PORTC_CR_R   = 0xFF;        // Enable overwrite of PUR to FP0
     GPIO_PORTD_CR_R   = 0xFF;        // Enable overwrite of PUR to FP0
+    GPIO_PORTF_CR_R   = 0xFF;        // Enable overwrite of PUR to FP0
 
     // Set port direction for the LCD display (all outputs).
     GPIO_PORTC_DIR_R |= 0xF0;
     GPIO_PORTD_DIR_R |= 0x0C;
+    GPIO_PORTF_DIR_R |= 0x0E;
 
     // Enable the digital funktion of the GPIO pins for the LCD display.
     GPIO_PORTC_DEN_R |= 0xF0;
     GPIO_PORTD_DEN_R |= 0x0C;
+    GPIO_PORTF_DEN_R |= 0x0E;
     // And the same thing for the RS and enable LCD pin
 
 }
@@ -86,10 +105,10 @@ void lcd_init(void)
     GPIO_PORTC_DATA_R &= ~(0xF0);       //slukker alle data porte
     GPIO_PORTD_DATA_R &= ~(0x0C);       //slukker RS og Enable
 
-    wait(5000);                         // wait 500ms
+    wait(500);                         // wait 500ms
 
     lcd_write4(LCD_FUNCTIONRESET);      //First try
-    wait_micro(2000);
+    wait_micro(40000);
 
     lcd_write4(LCD_FUNCTIONRESET);      //Second try
     wait_micro(400);
@@ -98,22 +117,23 @@ void lcd_init(void)
     wait_micro(400);
 
     lcd_write4(LCD_ENABLE_4BIT);        //Enable 4bit mode
-    wait_micro(4500);
+    wait_micro(160);
 
     lcd_write8(LCD_FUNCTION_SET);       //4 bit mode, 1/16, 5x8 font
-    wait_micro(4500);
+    wait_micro(160);
 
     lcd_write8(LCD_DISPLAY_OFF);        // Display off 
-    wait_micro(4500);
+    wait_micro(160);
 
     lcd_write8(LCD_DISPLAY_ON_BLINK);   // Display on with blinking cursor
-    wait_micro(4500);
+    wait_micro(160);
 
     lcd_write8(LCD_ENTRYMODE);          // Entry mode 
-    wait_micro(4500);
+    wait_micro(160);
 
-    lcd_write8(0x02);                   // Home
-    wait_micro(4500);
+    lcd_write8(LCD_HOME);                   // Home
+    wait_micro(160);
+
 }
 //******************* End of function *******************//
 
@@ -122,9 +142,9 @@ void lcd_write(int value)
 {
     // Enables the LCD_RS pin to set the LCD in data mode
     GPIO_PORTD_DATA_R |= LCD_RS;
-    wait(2);
+    wait_micro(200);
     lcd_write8(value);
-    wait(2);
+    wait_micro(20);
     GPIO_PORTD_DATA_R &= ~(LCD_RS);
 }
 //******************* End of function *******************//
@@ -132,10 +152,10 @@ void lcd_write(int value)
 // Sends a byte in two halvs.
 void lcd_write8(int value)
 {
-    GPIO_PORTC_DATA_R |= ( value && 0xF0 );     //Tænder for de nedre bits
+    GPIO_PORTC_DATA_R &= ~( 0xF0 );             // Makes sure that the bits are turned off.
+    GPIO_PORTC_DATA_R |= ( value );     //Tænder for de øvre bits
     pulse_enable();                             // Sender en puls på enable for at sende bits
     GPIO_PORTC_DATA_R &= ~( 0xF0 );             // Søger for at de bliver slukket igen
-
 
     GPIO_PORTC_DATA_R |= (value << 4);          //Tænder for de øvre bits
     pulse_enable();                             // Sender en puls på enable for at sende bits
@@ -148,10 +168,12 @@ void lcd_write8(int value)
 // If the LCD is in 8bit mode this command sends a 4bit value on the the highbits
 void lcd_write4(int value)
 {
-    GPIO_PORTC_DATA_R &= ~( 0xF0 );             // Søger for at de bliver slukket igen
-    GPIO_PORTC_DATA_R |= (value << 4) ;         // Tænder bits
-    pulse_enable();                             // Sender en puls på enable for at sende bits
-    GPIO_PORTC_DATA_R &= ~( 0xF0 );             // Søger for at de bliver slukket igen
+    GPIO_PORTD_DATA_R &= ~(LCD_RS);
+    GPIO_PORTC_DATA_R &= ~( 0xF0 );             // Makes sure that the bits are turned off.
+
+    GPIO_PORTC_DATA_R |= (value << 4);          // Left shift the bits to make them sit on the highbits.
+    pulse_enable();                             // Sends a pulse.
+    GPIO_PORTC_DATA_R &= ~( 0xF0 );             // Turns the bits off.
 }
 //******************* End of function *******************//
 
@@ -168,6 +190,7 @@ void pulse_enable(void)
 // Waits in microsecounds*time
 void wait_micro( int time )
 {                           //80Mhz CPU
+    time = 16*time;
     while (time--);
 }
 //******************* End of function *******************//
@@ -175,7 +198,7 @@ void wait_micro( int time )
 //This function is only used when it's an estimate
 void wait (int time)
 {
-    time = 2000*time;
+    time = 16000*time;
     while (time--);
 }
 //******************* End of function *******************//
